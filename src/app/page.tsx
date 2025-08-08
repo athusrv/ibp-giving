c99f3a0c2afeded4309eb7e6e2674b02cb6230c6
avieira <athusvieira@gmail.com>
2025-08-08 12:29:43 -0300

    lint fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,10 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
-import { Copy, MoveRight } from "lucide-react";
+import { MoveRight } from "lucide-react";
 import Image from "next/image";
-import { toast } from "sonner";
-
-const bankAccounts = [
-  {
-    BankImage: () => (
-      <div className="w-6 h-6 relative">
-        <Image fill src="/sicredi_black.svg" alt="logo_sicredi" />
-      </div>
-    ),
-    bank: "748 - Banco Sicredi",
-    accountName: "Igreja Batista da Pituba",
-    routingNumber: "0903",
-    accountNumber: "18377-2",
-    swift: "13.659.792/0001-52",
-    pix: "13.659.792/0001-52",
-  },
-  {
-    BankImage: () => (
-      <div className="w-6 h-6 relative">
-        <Image fill src="/bradesco_black.svg" alt="logo_bradesco" />
-      </div>
-    ),
-    bank: "237 - Banco Bradesco",
-    accountName: "Igreja Batista da Pituba",
-    routingNumber: "3646",
-    accountNumber: "5581-6",
-    swift: "13.659.792/0001-52",
-    pix: "tesouraria@ibpituba.com.br",
-  },
-];
-
-const anexoAccounts = [
-  {
-    BankImage: () => (
-      <div className="w-6 h-6 relative">
-        <Image fill src="/santander_black.svg" alt="logo_santander" />
-      </div>
-    ),
-    bank: "033 - Banco Santander",
-    accountName: "Igreja Batista da Pituba",
-    routingNumber: "3747",
-    accountNumber: "13006040-1",
-    swift: "13.659.792/0001-52",
-    pix: "anexo@ibpituba.com.br",
-  },
-];
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center py-8 px-4 bg-gray-50">
-      {/* Logo and Header Section */}
       <div className="flex flex-col items-center mb-12">
         <div className="w-40 h-40 relative">
           <Image src="/IBP.svg" alt="Logo" fill className="object-contain" />
@@ -71,7 +21,7 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col max-w-4xl w-full gap-2">
-        <a href="/giving">
+        <Link href="/giving">
           <div className="flex  items-center w-full p-4 rounded-lg border border-input bg-white gap-4 hover:bg-gray-50 transition cursor-pointer active:scale-99 active:bg-gray-100">
             <div className="flex flex-col justify-center flex-1">
               <h3 className="flex items-center gap-4 text-md font-bold leading-tight hidden lg:block">
@@ -87,8 +37,8 @@ export default function Home() {
             </div>
             <MoveRight />
           </div>
-        </a>
-        <a href="/anexo">
+        </Link>
+        <Link href="/anexo">
           <div className="flex  items-center w-full p-4 rounded-lg border border-input bg-white gap-4 hover:bg-gray-50 transition cursor-pointer active:scale-99 active:bg-gray-100">
             <div className="flex flex-col justify-center flex-1">
               <h3 className="flex items-center gap-4 text-md font-bold leading-tight hidden lg:block">
@@ -103,7 +53,7 @@ export default function Home() {
             </div>
             <MoveRight />
           </div>
-        </a>
+        </Link>
       </div>
     </main>
   );
